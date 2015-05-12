2adc8ae4c1d0a6423ccd951e87a9e06459f09d99
Benjamin Anderson <me@bander.so>
2015-05-11 22:29:11 -0700

    libinput added
    Started work on reading raw keyboard input.

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ src = $(wildcard ./src/*.c)
 obj = $(subst src,obj,$(src:.c=.o))
 
 CFLAGS = -g -Wall -pedantic -Wno-deprecated-declarations -Wno-missing-braces
-LDFLAGS = -L./lib/noise -framework audiotoolbox -framework corefoundation
+LDFLAGS = -L./lib/noise -L./lib/input -framework audiotoolbox -framework iokit -framework corefoundation
 
-audio: obj $(obj) libnoise
-	$(CC) $(CFLAGS) -o $@ $(obj) -lnoise $(LDFLAGS)
+audio: obj $(obj) libnoise libinput
+	$(CC) $(CFLAGS) -o $@ $(obj) -lnoise -linput $(LDFLAGS)
 
 obj:
 	mkdir obj
@@ -13,9 +13,12 @@ obj:
 obj/%.o: src/%.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
-.PHONY: libnoise
+.PHONY: libnoise libinput
 libnoise:
 	@$(MAKE) -s -C ./lib/noise
+
+libinput:
+	@$(MAKE) -s -C ./lib/input
 
 .PHONY: clean print-% check-syntax 
 clean:
--- src/audio.c
+++ src/audio.c
@@ -3,12 +3,27 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <AudioToolbox/AudioFile.h>
 #include <CoreFoundation/CFURL.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
 
 #include "../lib/noise/src/noise.h"
+#include "../lib/input/src/input.h"
 
 #define internal static
 #define global static
 #define local static
+
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef uint16_t u16;
+typedef int16_t i16;
+typedef uint32_t u32;
+typedef int32_t i32;
+typedef uint64_t u64;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
 
 global const float kTau = 2.0f * M_PI;
 enum {
@@ -38,6 +53,39 @@ typedef enum MusicalNote_ {
   Aflat = Gsharp
 } MusicalNote;
 
+typedef enum ADSRState_ {
+  IDLE = 0,
+  ATTACK,
+  DECAY,
+  SUSTAIN,
+  RELEASE
+} ADSRState;
+
+typedef struct ADSR_ {
+  ADSRState state;
+  bool gate;
+
+  f32 targetRatioA;
+  
+  f32 attackRate;
+  f32 attackCoef;
+  f32 attackBase;
+
+  f32 targetRatioDR;
+  
+  f32 decayRate;
+  f32 decayCoef;
+  f32 decayBase;
+  
+  f32 releaseRate;
+  f32 releaseCoef;
+  f32 releaseBase;
+
+  f32 sustainLevel;
+
+  f32 output;
+} ADSR, *pADSR;
+
 typedef struct AQPlayerState_ {
   AudioStreamBasicDescription  mDataFormat;
   AudioQueueRef                mQueue;
@@ -47,8 +95,154 @@ typedef struct AQPlayerState_ {
   SInt64                       mCurrentPacket;
   UInt32                       mNumPacketsToRead;
   AudioStreamPacketDescription *mPacketDescs;
+  pADSR                        adsr;
   bool                         mIsRunning;
 } AQPlayerState;
+
+global struct termios gOriginalTermios;
+internal void reset_terminal_mode(void) {
+  tcsetattr(0, TCSANOW, &gOriginalTermios);
+}
+
+internal void set_raw_terminal_mode(void) {
+  struct termios newTermios;
+
+  // store termios settings for restoring later
+  tcgetattr(0, &gOriginalTermios);
+  memcpy(&newTermios, &gOriginalTermios, sizeof(newTermios));
+
+  // restore orignal termios at exit
+  atexit(reset_terminal_mode);
+
+  // make the termios raw
+  cfmakeraw(&newTermios);
+  tcsetattr(0, TCSANOW, &newTermios);
+}
+
+internal int kbhit(void) {
+  struct timeval tv = { 0L, 0L};
+  fd_set fds;
+  FD_ZERO(&fds);
+  FD_SET(0, &fds);
+  return select(1, &fds, NULL, NULL, &tv);
+}
+
+internal u8 get_key(void) {
+  ssize_t r = -1;
+  u8 c = 0;
+  if ((r = read(0, &c, sizeof(c))) < 0) {
+    return 0;
+  } else {
+    return c;
+  }
+}
+
+internal f32 calcCoef(f32 rate, f32 targetRatio) {
+    return expf(-logf((1.0f + targetRatio) / targetRatio) / rate);
+}
+
+internal void adsr_attack(pADSR adsr, f32 rate) {
+  adsr->attackRate = rate;
+  adsr->attackCoef = calcCoef(rate, adsr->targetRatioA);
+  adsr->attackBase = (1.0f + adsr->targetRatioA) * (1.0f - adsr->attackCoef);
+}
+
+internal void adsr_decay(pADSR adsr, f32 rate) {
+  adsr->decayRate = rate;
+  adsr->decayCoef = calcCoef(rate, adsr->targetRatioDR);
+  adsr->decayBase = (adsr->sustainLevel - adsr->targetRatioDR) * (1.0f - adsr->decayCoef);
+}
+
+internal void adsr_release(pADSR adsr, f32 rate) {
+  adsr->releaseRate = rate;
+  adsr->releaseCoef = calcCoef(rate, adsr->targetRatioDR);
+  adsr->releaseBase = -adsr->targetRatioDR * (1.0f - adsr->releaseCoef);
+}
+
+internal void adsr_sustain(pADSR adsr, f32 level) {
+  adsr->sustainLevel = level;
+  adsr->decayBase = (adsr->sustainLevel - adsr->targetRatioDR) * (1.0f - adsr->decayCoef);
+}
+
+internal void adsr_targetRatioA(pADSR adsr, f32 ratio) {
+  if (ratio < 0.000000001f) {
+    ratio = 0.000000001f; // -180 dB
+  }
+  adsr->targetRatioA = ratio;
+  adsr->attackBase = (1.0f + adsr->targetRatioA) * (1.0f - adsr->attackCoef);
+}
+
+internal void adsr_targetRatioDR(pADSR adsr, f32 ratio) {
+  if (ratio < 0.000000001f) {
+    ratio = 0.000000001f; // -180 dB
+  }
+  adsr->targetRatioDR = ratio;
+  adsr->decayBase = (adsr->sustainLevel - adsr->targetRatioDR) * (1.0f - adsr->decayCoef);
+  adsr->releaseBase = -adsr->targetRatioDR * (1.0f - adsr->releaseCoef);
+}
+
+internal f32 adsr_process(pADSR adsr) {
+  f32 output = adsr->output;
+  switch (adsr->state) {
+    case IDLE: break;
+    case ATTACK: {
+      output = adsr->attackBase + output * adsr->attackCoef;
+      if (output >= 1.0f) {
+        output = 1.0f;
+        adsr->state = DECAY;
+        printf("adsr: DECAY\n");
+      }
+    } break;
+    case DECAY: {
+      output = adsr->decayBase + output * adsr->decayCoef;
+      if (output <= adsr->sustainLevel) {
+        output = adsr->sustainLevel;
+        adsr->state = SUSTAIN;
+        printf("adsr: SUSTAIN\n");
+      }
+    } break;
+    case SUSTAIN: break;
+    case RELEASE: {
+      output = adsr->releaseBase + output * adsr->releaseCoef;
+      if (output <= 0.0f) {
+        output = 0.0f;
+        adsr->state = IDLE;
+        printf("adsr: IDLE\n");
+      }
+    } break;
+  }
+  adsr->output = output;
+  //printf("adsr: %f\n", output);
+  return output;
+}
+
+internal void adsr_gate(pADSR adsr) {
+  if (!adsr->gate) {
+    adsr->state = ATTACK;
+    adsr->gate = true;
+    printf("adsr: ATTACK\n");
+  } else if (adsr->state != IDLE) {
+    adsr->state = RELEASE;
+    adsr->gate = false;
+    printf("adsr: RELEASE\n");
+  }
+}
+
+internal void adsr_reset(pADSR adsr) {
+  adsr->gate = false;
+  adsr->state = IDLE;
+  adsr->output = 0.0f;
+}
+
+internal void adsr_init(pADSR adsr) {
+  adsr_reset(adsr);
+  adsr_attack(adsr, 0.0f);
+  adsr_decay(adsr, 0.0f);
+  adsr_release(adsr, 0.0f);
+  adsr_sustain(adsr, 1.0f);
+  adsr_targetRatioA(adsr, 0.3f);
+  adsr_targetRatioDR(adsr, 0.0001f);
+}
 
 internal float SineWave(float amplitude, float period, float phase) {
   return amplitude * sinf((kTau / period) + phase);
@@ -90,6 +284,8 @@ internal void SynthCallback(void *aqData,
   SInt16 *coreAudioBuffer = (SInt16 *)outBuffer->mAudioData;
   outBuffer->mAudioDataByteSize = kBufferSizeInFrames * pAqData->mDataFormat.mBytesPerFrame;
 
+  pADSR adsr = pAqData->adsr;
+
   float volumeL = gVolume;
   float volumeR = gVolume;
   float phaseL = gPhase;
@@ -97,9 +293,13 @@ internal void SynthCallback(void *aqData,
   float fStepL = gFStep;
   float fStepR = gFStep;
 
+  float envelope = 0.0f;
   for (int s = 0; s < kBufferSizeInFrames * 2; s +=2) {
-    float sampleL = SineWave(volumeL, fStepL, phaseL);
-    float sampleR = SineWave(volumeR, fStepR, phaseR);
+    if (adsr->gate) {
+      envelope = adsr_process(adsr) + 1.0f;
+    }
+    float sampleL = SineWave(volumeL, fStepL, phaseL) * envelope;
+    float sampleR = SineWave(volumeR, fStepR, phaseR) * envelope;
 
     short sampleIL = (short)(sampleL * 32767.0f);
     short sampleIR = (short)(sampleR * 32767.0f);
@@ -127,7 +327,14 @@ internal float CalcFrequencyFromNote(MusicalNote semiTones, SInt32 octave) {
   return powf(2.0f, (float)semiTones / 12.0f) * root;
 }
 
-internal void PlaySine(void) {
+internal void PlaySynth(void) {
+  ADSR adsr = {0};
+  adsr_init(&adsr);
+  adsr_attack(&adsr, 0.1f * kSampleRate);
+  adsr_decay(&adsr, 0.3f * kSampleRate);
+  adsr_sustain(&adsr, 0.8f);
+  adsr_release(&adsr, 5.0f * kSampleRate);
+  
   AQPlayerState aqData = {0};
   aqData.mDataFormat.mSampleRate = kSampleRate;
   aqData.mDataFormat.mFormatID = kAudioFormatLinearPCM;
@@ -137,6 +344,8 @@ internal void PlaySine(void) {
   aqData.mDataFormat.mBytesPerPacket = aqData.mDataFormat.mBytesPerFrame = sizeof(SInt16) * 2;
   aqData.mDataFormat.mBitsPerChannel = 16;
   aqData.mDataFormat.mReserved = 0;
+  
+  aqData.adsr = &adsr;
 
   SetWave(CalcFrequencyFromNote(C, 4), 0.5f);
 
@@ -167,11 +376,33 @@ internal void PlaySine(void) {
   result = AudioQueueStart(aqData.mQueue, NULL);
   if (result) return;
 
+  const u8 gate_key = 'g';
+  const u8 esc_key = 27;
+  u8 key = 0;
+  bool keyIsDown = false;
+  //set_raw_terminal_mode();
+  const float loopTime = 1.0f / 60.0f;
+  input();
   do {
-    CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.25f, false);
+    CFRunLoopRunInMode(kCFRunLoopDefaultMode, loopTime, false);
+    /* if (kbhit()) { */
+    /*   key = get_key(); */
+    /*   if (key == gate_key) { */
+    /*     printf("%c", key); */
+    /*     fflush(stdout); */
+    /*     if (keyIsDown) { */
+    /*     } else { */
+    /*       adsr_gate(&adsr); */
+    /*     } */
+    /*   } else if (key == esc_key) { */
+    /*     aqData.mIsRunning = false; */
+    /*   } else { */
+    /*     key = 0; */
+    /*   } */
+    /* } */
   } while (aqData.mIsRunning);
 
-  CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0f, false);
+  CFRunLoopRunInMode(kCFRunLoopDefaultMode, loopTime, false);
 
   AudioQueueStop(aqData.mQueue, true);
 }
@@ -337,7 +568,7 @@ int main(int argc, char** args) {
   perlin_init();
   
   if (argc < 2) {
-    PlaySine();
+    PlaySynth();
   } else {
     PlayFile(args[1]);
   }
